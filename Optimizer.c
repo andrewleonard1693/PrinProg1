044f6f0c872a48383e815ed30171a22a9ac00ddd
aj <andrewleonard1693@gmail.com>
2018-03-04 21:54:39 -0500

    implemented switch cases for opcodes

--- Optimizer.c
+++ Optimizer.c
@@ -7,8 +7,31 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "InstrUtils.h"
 #include "Utils.h"
+
+int findNumberOfInstructions(Instruction *head){
+	int count =0;
+	Instruction *ptr = head;
+	while(ptr!=NULL){
+		ptr=ptr->next;
+		count+=1;
+	}
+	return count;
+}
+int checkCritical(int val, int arr[]);
+
+int checkCritical(int val, int arr[]) {
+    int len = sizeof(arr)/sizeof(int);
+    for(int i = 0;i<len;++i){
+        if(arr[i]==val){
+            return 1;
+        }
+    }
+    return -1;
+}
+
 
 int main()
 {
@@ -22,10 +45,65 @@ int main()
 	/* YOUR CODE GOES HERE */
 	if (head) {
 		Instruction *ptr = head;
+		//get tail of instructions
+		Instruction *tail = LastInstruction(head);
+		//get the number of instructions present in the linked list
+		int numInstructions = findNumberOfInstructions(head);
+		//set an index for inserting into an array
+		int arrayIndex = 0;
+		//create an array to represent the critical registers
+		int critical[numInstructions];
+		//set a pointer to the tail to iterate backwards
+		Instruction *ptr = tail;
 		while(ptr!=NULL){
-			printf("Instruction critical = %c\n",ptr->critical);
-			ptr=ptr->next;
+			switch(ptr->opcode){
+				//if the opcode is WRITE then we push field1 into the arrary of criticals
+				case WRITE: //WRITE
+					ptr->critical='y';
+					//subtract by 200 to make the value of the variable negative
+					critical[arrayIndex]=(ptr->field1)-200;
+					arrayIndex+=1;
+					break;
+				case READ: //READ
+					ptr->critical='y';
+					break;
+				case STORE: //STORE
+					if(checkCritical((ptr->field1)-200,critical)==1){
+						critical[arrayIndex]=ptr->field2;
+						arrayIndex+=1;
+						ptr->critical='y';
+					}
+					else{
+						ptr->critical='n';
+					}
+					break;
+				case LOADI:
+				case LOAD:
+					if(checkCritical(ptr->field1,critical)==1){
+						critical[arrayIndex]=ptr->field1;
+						arrayIndex+=1;
+						ptr->critical='y';
+					}else{
+						ptr->critical='n';
+					}
+				case ADD:
+				case MUL:
+				case SUB:
+					if(checkCritical(ptr->field1,critical)==1){
+						critical[arrayIndex]=ptr->field2;
+						arrayIndex+=1;
+						critical[arrayIndex]=ptr->field3;
+						arrayIndex+=1;
+						ptr->critical='y';
+					}else{
+						ptr->critical='n';
+					}
+			}
+			ptr=ptr->prev;
 		}
+
+		//loop through the linked list and delete all of the nodes that are not critical
+		
 		PrintInstructionList(stdout, head);
 		DestroyInstructionList(head);
 	}
